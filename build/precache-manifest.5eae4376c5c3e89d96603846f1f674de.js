8bbc1e313ed826f972d62bc9ac8cd4a48dd3b7dd
Joe Thompson <smnindian30@yahoo.com>
2020-12-07 16:26:05 -0600

    production build for client

---
+++ build/precache-manifest.5eae4376c5c3e89d96603846f1f674de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a0e27bb160a56eca213c67fd63f18b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae4540f75d67849b9583",
+    "url": "/static/css/main.8ac53766.chunk.css"
+  },
+  {
+    "revision": "e2a26c41d409d76193ac",
+    "url": "/static/js/2.830a161d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.830a161d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4540f75d67849b9583",
+    "url": "/static/js/main.d6b3a5f8.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
